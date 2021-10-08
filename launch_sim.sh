80379fbc882ca4f73740aacf57add4094c3cd3c4
hgray2 <hgray576@gmail.com>
2021-10-08 19:33:49 -0400

    Updated simulation.launch
    simulation.launch now launches unity
    Added a bash file for launching the sim

---
+++ launch_sim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This must be updated along side the editor version used for the simulator.
+unity_version="2020.3.17f1"
+
+# Ensure that your editors are installed at this directory!
+/home/$USER/Unity/Hub/Editor/$unity_version/Editor/Unity -projectPath "/home/$USER/Unity/riptide_simulator"
