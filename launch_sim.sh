dc6018684f858029b455f178ae6bf4169f322ae8
Seth <aldridge.94@osu.edu>
2021-10-24 13:32:12 -0400

    Roslaunch simulation.launch now starts nodes without opening gazebo

--- launch_sim.sh
+++ launch_sim.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Write user's IP to params.yaml
+USER_IP=`hostname -I | cut -f1 -d' '`
+cd /home/$USER/osu-uwrt/riptide_software/src/ROS-TCP-Endpoint/config/
+echo "ROS_IP: $USER_IP" > params.yaml
+rosparam set ROS_IP $USER_IP
+
 # This must be updated along side the editor version used for the simulator.
 unity_version="2020.3.17f1"
 
