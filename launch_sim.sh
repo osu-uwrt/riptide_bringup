9fa81814ccab6e1d626a2e505cb7b59e34d33936
Seth <aldridge.94@osu.edu>
2021-10-27 16:47:35 -0400

    Changed simulation.launch to target gazebo

--- launch_sim.sh
+++ launch_sim.sh
@@ -2,9 +2,12 @@
 
 # Write user's IP to params.yaml
 USER_IP=`hostname -I | cut -f1 -d' '`
-cd /home/$USER/osu-uwrt/riptide_software/src/ROS-TCP-Endpoint/config/
-echo "ROS_IP: $USER_IP" > params.yaml
+echo "ROS_IP: $USER_IP" > ../riptide_gazebo/config/params.yaml
 rosparam set ROS_IP $USER_IP
+
+# TODO: Add automatic assignment of IP to ROSConnectionPrefab
+# LINE_NUM=`grep -n "m_RosIPAddress" /home/$USER/Unity/riptide_simulator/Assets/Resources/ROSConnectionPrefab.prefab`
+# sed -i "s/.*m_RosIPAddress.*/m_RosIPAddress ${LINE_NUM}./" /home/$USER/Unity/riptide_simulator/Assets/Resources/ROSConnectionPrefab.prefab
 
 # This must be updated along side the editor version used for the simulator.
 unity_version="2020.3.17f1"
