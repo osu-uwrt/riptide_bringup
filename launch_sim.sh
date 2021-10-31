8b999277412d48fa8b57f41fe1856502c4e5b295
hgray2 <hgray576@gmail.com>
2021-10-31 13:42:19 -0400

    Automatic IP assignment for TCP bridge

--- launch_sim.sh
+++ launch_sim.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Write user's IP to params.yaml
-USER_IP=`hostname -I | cut -f1 -d' '`
-echo "ROS_IP: $USER_IP" > ../riptide_gazebo/config/params.yaml
-rosparam set ROS_IP $USER_IP
 
-# TODO: Add automatic assignment of IP to ROSConnectionPrefab
-# LINE_NUM=`grep -n "m_RosIPAddress" /home/$USER/Unity/riptide_simulator/Assets/Resources/ROSConnectionPrefab.prefab`
-# sed -i "s/.*m_RosIPAddress.*/m_RosIPAddress ${LINE_NUM}./" /home/$USER/Unity/riptide_simulator/Assets/Resources/ROSConnectionPrefab.prefab
+
+
+
+
+# Automatic assignment of host IP for TCP connection
+USER_IP=`hostname -I | cut -f1 -d' '`
+echo "ROS_IP: $USER_IP" > ../riptide_gazebo/config/params.yaml # Write user's IP to params.yaml
+rosparam set ROS_IP $USER_IP
+sed -i "s/.*m_RosIPAddress.*/  m_RosIPAddress: ${USER_IP}/" /home/$USER/Unity/riptide_simulator/Assets/Resources/ROSConnectionPrefab.prefab
 
 # This must be updated along side the editor version used for the simulator.
 unity_version="2020.3.17f1"
